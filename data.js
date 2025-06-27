87ee9f3389d4bcf3e1f79966b1bf98e28259a49d
Evan <haotor@protonmail.com>
2025-06-27 15:33:04 +0800

    重新上传网页源码

---
+++ data.js
@@ -0,0 +1,8 @@
+// 模拟假数据展示用
+document.getElementById('okx-buy').innerText = '7.22';
+document.getElementById('binance-buy').innerText = '7.23';
+document.getElementById('huobi-buy').innerText = '7.21';
+
+document.getElementById('okx-sell').innerText = '7.26';
+document.getElementById('binance-sell').innerText = '7.27';
+document.getElementById('huobi-sell').innerText = '7.25';
